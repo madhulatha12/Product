ea1af9e0258a0bcb0a1e8c89b5fae2a2e7fae3e1
Madhu Latha <madhulathachinda@gmail.com>
2022-01-21 08:59:41 +0530

    this the springboot crud

--- src/main/java/com/product/controller/ProductController.java
+++ src/main/java/com/product/controller/ProductController.java
@@ -1,8 +1,8 @@
 package com.product.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,37 +12,61 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.product.demo.Product;
+import com.product.exception.ProductNotFoundException;
 import com.product.service.ProductService;
+
 @RestController
 public class ProductController {
 	@Autowired
 	private ProductService service;
 	
+	@GetMapping("/product/{id}")
+	public ResponseEntity<Object> getProduct(@PathVariable("id") int id ) { 
+	  Product p= service.getProduct(id);
+	  
+	  if(p==null)
+		  throw new ProductNotFoundException();
+	   return new ResponseEntity<>(p, HttpStatus.OK);
+	 }
 	@PostMapping("/product")
-	public Product insertproduct(@RequestBody Product p) {
-		return service.addProduct(p);
-		
-		
-	}
-	@GetMapping("/product")
-	public List<Product> getAllProducts() {
-		return service.getAllProducts();
-		
-	}
-	@PutMapping("/product")
-	public Product updateproduct(@RequestBody Product p) {
-		return service.addProduct(p);
-		
-	}
+	public ResponseEntity<Object> insertproduct(@RequestBody Product p)
+	  {
+		Product p1= service.addProduct(p);
+		if(p1==null)
+			  throw new ProductNotFoundException();
+		   return new ResponseEntity<>("product added sucessfully", HttpStatus.OK);
+	 
+	 
+	 }
+	@PutMapping("/product") 
+	public ResponseEntity<Object> updateproduct(@RequestBody Product p)
+	  { 
+	     Product p1=service.updateProduct(p);
+	     if(p1==null)
+			  throw new ProductNotFoundException();
+		   return new ResponseEntity<>("product updated sucessfully", HttpStatus.OK);
+	 
+	 
+	 }
 	@DeleteMapping("/product/{id}")
-	public Product deleteproduct(@PathVariable("id") int id) {
-		return service.deleteProduct(id);
-		
+	public ResponseEntity<Object> deleteproduct(@PathVariable("id") int id) 
+	{ 
+	     Product p1=service.deleteProduct(id);
+	     if(p1==null)
+			  throw new ProductNotFoundException();
+		   return new ResponseEntity<>("product deleted sucessfully", HttpStatus.OK);
+	 
+	 
+	 }
+	  
+	  }
+	
+	
+	
+	
 
-}
-}
 	
-	
+
 	
 
 
--- src/main/java/com/product/service/ProductService.java
+++ src/main/java/com/product/service/ProductService.java
@@ -5,21 +5,67 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+
 import com.product.demo.Product;
 import com.product.repository.ProductRepository;
+
 @Service
 public class ProductService {
+	
 	@Autowired
 	private ProductRepository repo;
+	
 	public Product addProduct(Product p) {
+		
 		return repo.save(p);
 	}
+	
+public Product updateProduct(Product p) {
+		
+		return repo.save(p);
+	}
+	
 	public List<Product> getAllProducts(){
+		
 		return (List<Product>)repo.findAll();
+		
 	}
-	public Product deleteProduct(int address) {
-		Product p=repo.findById(address).get();
-		repo.deleteById(address);
-		return p;
+	
+	public Product deleteProduct(int id) {
+		
+			Product p=repo.findById(id).get();
+			repo.deleteById(id);
+			return p;
+		}
+		
+		//System.out.println("address="+address);
+		//Product p=null;
+		//try {
+			// p=repo.findById(address).get();
+			
+	//	}
+		//catch(Exception e) {
+		//	throw new ProductNotFoundException("address:"+address);
+			
+		
+		//}
+		
+		//System.out.println("p="+p);
+	//	if(p!=null)
+	//	repo.deleteById(address);
+		//return p;
+	
+
+
+	public Product getProduct(int id) {
+		
+		return repo.findById(id).orElse(null);
 	}
-}
+
+
+	
+
+
+	
+	}
+
